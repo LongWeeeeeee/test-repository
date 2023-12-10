7fbc402ef736ea958c10cce1030fed379395f3a1
LongWeeeeeee <127714371+LongWeeeeeee@users.noreply.github.com>
2023-12-10 15:36:36 +0300

    jashdk

--- Dailies.py
+++ Dailies.py
@@ -1,7 +1,6 @@
 import asyncio
 from sqlite import db_start, create_profile, edit_database
-from openpyxl import load_workbook
-from datetime import datetime, timedelta
+from datetime import datetime
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
@@ -9,13 +8,11 @@ from aiogram.types import Message
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from aiogram import types
 from aiogram import F
-import copy
+from fuctions import create_main_keyboard, diary_out, create_settings_keyboard, create_jobs_keyboard, add_day_to_excel
 from aiogram.filters import Command
-import pandas as pd
 from aiogram.types import FSInputFile
 import json
 import os
-from openpyxl.workbook import Workbook
 import keys
 
 bot = Bot(token=keys.Token)
@@ -46,18 +43,14 @@ async def on_startup():
 
 @dp.message(Command(commands=["start"]))
 async def greetings(message: Message, state: FSMContext):
-    # await create_profile(user_id=message.from_user.id)
     answer = await create_profile(user_id=message.from_user.id)
     if answer is not None:
         if answer[1] != '':
-            daily_scores = json.loads(answer[1])
-            await state.update_data(daily_scores=daily_scores)
+            await state.update_data(daily_scores=json.loads(answer[1]))
         if answer[2] != '':
-            one_time_jobs = json.loads(answer[2])
-            await state.update_data(one_time_jobs=one_time_jobs)
+            await state.update_data(one_time_jobs=json.loads(answer[2]))
         if answer[3] != '':
-            date_jobs = json.loads(answer[3])
-            await state.update_data(date_jobs=date_jobs)
+            await state.update_data(date_jobs=json.loads(answer[3]))
         print(answer)
         await existing_user(message, state)
     else:
@@ -284,7 +277,7 @@ async def process_one_time(message: Message, state: FSMContext):
             one_time_jobs.remove(jobs.lower().replace('ё', 'е'))
     await edit_database(one_time_jobs=one_time_jobs)
     await state.update_data(one_time_jobs=one_time_jobs)
-    if one_time_jobs == []:
+    if not one_time_jobs:
         await message.answer('Поздравляю! Вы выполнили все разовые дела, так держать')
     await message.answer("Сколько сделал шагов?")
     await state.set_state(ClientState.steps)
@@ -336,144 +329,6 @@ async def process_unknown_write_bots(message: Message, state: FSMContext):
     await state.set_state(ClientState.greet)
 
 
-async def add_day_to_excel(date, activities, user_message, total_sleep, deep_sleep, rate, mysteps, message,
-                           daily_scores,
-                           user_name):
-    # Загрузка существующего файла Excel
-    # with open('daily_scores.txt', 'r+', encoding='utf-8-sig') as f:
-    #     line = json.load(f)
-    #     for user in line:
-    #         if message.from_user.id == user['user_id']:
-    #             daily_scores = user['daily_scores']
-    if os.path.exists(f'{user_name}_Diary.xlsx'):
-        # If the file exists, load the existing workbook
-        wb = load_workbook(f'{user_name}_Diary.xlsx')
-    else:
-        # If the file doesn't exist, create a new workbook
-        wb = Workbook()
-        sheet = wb.active
-        sheet.cell(row=1, column=1).value = 'Дата'
-        sheet.cell(row=1, column=2).value = 'Дела за день'
-        sheet.cell(row=1, column=3).value = 'Шаги'
-        sheet.cell(row=1, column=4).value = 'Total sleep'
-        sheet.cell(row=1, column=5).value = 'Deep sleep'
-        sheet.cell(row=1, column=6).value = 'О дне'
-        sheet.cell(row=1, column=7).value = 'My rate'
-        sheet.cell(row=1, column=8).value = 'Total'
-
-    # Получение активного листа
-    sheet = wb.active
-
-    # Поиск последней заполненной строки в столбце "Дата"
-    last_row = sheet.max_row
-
-    # Получение вчерашней даты
-    yesterday = date - timedelta(days=1)
-
-    # Вставка вчерашней даты в следующую за последней заполненной строкой
-    sheet.cell(row=last_row + 1, column=1).value = yesterday.strftime("%d.%m.%Y")
-
-    # Вставка дел в таблицу
-    if type(activities) == list:
-        sheet.cell(row=last_row + 1, column=2).value = ", ".join(activities)
-    else:
-        sheet.cell(row=last_row + 1, column=2).value = activities
-    # Вставка шаги
-    sheet.cell(row=last_row + 1, column=3).value = mysteps
-    # Вставка сколько часов спал
-    sheet.cell(row=last_row + 1, column=4).value = total_sleep
-    # Вставка времени глубокого сна
-    sheet.cell(row=last_row + 1, column=5).value = deep_sleep
-    # Вставка о дне в таблицу
-    sheet.cell(row=last_row + 1, column=6).value = user_message
-    # Вставка времени глубокого сна
-    sheet.cell(row=last_row + 1, column=7).value = rate
-
-    # Вычисление итогового счета
-    score = 0
-    if type(activities) == list:
-        for activity in activities:
-            score += int(daily_scores[activity])
-    else:
-        score += int(daily_scores[activities])
-
-    # Вставка итогового счета в таблицу
-    sheet.cell(row=last_row + 1, column=8).value = score
-
-    # Сохранение изменений в файл
-    wb.save(f'{user_name}_Diary.xlsx')
-    kb = [[types.KeyboardButton(text="Скачать дневник"), types.KeyboardButton(text="Изменить Настройки"),
-           types.KeyboardButton(text="Вывести дневник")]]
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-    )
-
-    async def counter_negavite(current_word):
-        data = pd.read_excel(f'{user_name}_Diary.xlsx')
-
-        # Выбор нужной колонки
-        column = data['Дела за день']
-
-        def perebor(count=0):
-            for words in column.iloc[-1::-1]:
-                for word in words.split(', '):
-                    if word == current_word:
-                        return count
-                count += 1
-            return count
-        perebor()
-    async def counter_positive(current_word):
-        data = pd.read_excel(f'{user_name}_Diary.xlsx')
-        # Выбор нужной колонки
-        column = data['Дела за день']
-
-        def prohod(count=0):
-            for words in column.iloc[-1::-1]:
-                split_words = words.split(', ')
-                if current_word in split_words:
-                    count += 1
-                else:
-                    if count >= 2: return count
-
-            if count >= 2: return count
-
-        prohod()
-
-    total_days = dict()
-    for current_word in daily_scores:
-        counter_days = await counter_negavite(current_word)
-        if counter_days is not None and counter_days >= 3:
-            if counter_days in [2, 3, 4]:
-                total_days[current_word] = str(counter_days) + ' дня'
-            else:
-                total_days[current_word] = str(counter_days) + ' дней'
-    output = ['{}: {}'.format(key, value) for key, value in total_days.items()]
-    result = '\n'.join(output)
-    if result != '':
-        await message.answer(f'Вы не делали эти дела уже столько дней:\n{result}\nМожет стоит дать им еще один шанс?')
-    total_days = dict()
-    for current_word in activities:
-        counter_days = await counter_positive(current_word)
-        if counter_days in [2, 3, 4] and counter_days is not None:
-            total_days[current_word] = str(counter_days) + ' дня'
-        elif counter_days is not None:
-            total_days[current_word] = str(counter_days) + ' дней'
-    output = ['{}: {}'.format(key, value) for key, value in total_days.items()]
-    result = '\n'.join(output)
-    if result != '':
-        await message.answer('Поздравляю! Вы соблюдаете эти дела уже столько дней: ' + '\n' + result,
-                             reply_markup=keyboard)
-    else:
-        await message.answer('Дневник заполнен!', reply_markup=keyboard)
-
-
-def write_to_file(data, f):
-    f.truncate(0)
-    f.seek(0)
-    json.dump(data, f, ensure_ascii=False, indent=4)
-
-
 async def existing_user(message, state):
     user_data = await state.get_data()
     if 'daily_scores' in user_data:
@@ -491,48 +346,6 @@ async def existing_user(message, state):
 
         await message.answer(', '.join(one_time_jobs))
     await state.set_state(ClientState.greet)
-
-
-def create_main_keyboard():
-    kb = [[types.KeyboardButton(text="Скачать дневник"),
-           types.KeyboardButton(text="Изменить Настройки"),
-           types.KeyboardButton(text="Вывести дневник")]]
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-    )
-    return keyboard
-
-
-def create_change_settings_keyboard():
-    kb = [[types.KeyboardButton(text="Изменить Настройки")]]
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-    )
-    return keyboard
-
-
-def create_settings_keyboard():
-    kb = [[types.KeyboardButton(text="Изменить Дела"),
-           types.KeyboardButton(text='''Что такое "стоимость"?'''), types.KeyboardButton(text="Изменить Стоимость"),
-           types.KeyboardButton(text="Заполнить дневник")]]
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-    )
-    return keyboard
-
-
-def create_jobs_keyboard():
-    kb = [[types.KeyboardButton(text="Ежедневные дела"),
-           types.KeyboardButton(text="Разовые дела"),
-           types.KeyboardButton(text='В определенную дату')]]
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb,
-        resize_keyboard=True,
-    )
-    return keyboard
 
 
 async def handle_new_user(message: Message, state):
@@ -558,24 +371,6 @@ def start_scheduler(message, state):
         scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
         scheduler.add_job(greetings, 'cron', hour=8, minute=00, args=(message, state))
         scheduler.start()
-
-
-async def diary_out(message):
-    data = pd.read_excel(f'{message.from_user.username}_Diary.xlsx')
-    await message.answer(
-        "{} | {} | {} | {} | {} | {} | {} | {}".format("Дата", "Дела за день", "Шаги", "Total sleep", 'Deep sleep',
-                                                       'О дне', 'My rate', 'Total'))
-    for index, row in data.iterrows():
-        message_sheet = "{} | {} | {} | {} | {} | {} | {} | {}".format(row["Дата"], row["Дела за день"], row["Шаги"],
-                                                                       row["Total sleep"], row['Deep sleep'],
-                                                                       row['О дне'],
-                                                                       row['My rate'], row['Total'])
-
-        # разделение длинного сообщения на части
-        message_parts = [message_sheet[i:i + 4096] for i in range(0, len(message_sheet), 4096)]
-
-        for part in message_parts:
-            await message.answer(part)
 
 
 async def main():
