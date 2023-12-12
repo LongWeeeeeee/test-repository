0f2a977707582f3b5f22aaf32070cc5139c17986
LongWeeeeeee <127714371+LongWeeeeeee@users.noreply.github.com>
2023-12-12 22:22:02 +0300

    jashdk

--- Dailies.py
+++ Dailies.py
@@ -1,15 +1,16 @@
 import asyncio
+import copy
 from sqlite import db_start, create_profile, edit_database
-from datetime import datetime
+from datetime import datetime, timedelta
 from aiogram import Bot, Dispatcher
+from aiogram.filters.command import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import Message
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from aiogram import types
 from aiogram import F
-from fuctions import create_main_keyboard, diary_out, create_settings_keyboard, create_jobs_keyboard, add_day_to_excel
-from aiogram.filters import Command
+from fuctions import create_main_keyboard, diary_out, create_settings_keyboard, create_jobs_keyboard, add_day_to_excel, create_date_job_keyboard
 from aiogram.types import FSInputFile
 import json
 import os
@@ -35,11 +36,7 @@ class ClientState(StatesGroup):
     one_time_jobs_proceed = State()
     date_jobs = State()
     date_jobs_2 = State()
-
-
-async def on_startup():
-    await db_start()
-
+    date_jobs_3 = State()
 
 @dp.message(Command(commands=["start"]))
 async def greetings(message: Message, state: FSMContext):
@@ -55,7 +52,8 @@ async def greetings(message: Message, state: FSMContext):
         await existing_user(message, state)
     else:
         await handle_new_user(message, state)
-    start_scheduler(message, state)
+    already_started = False
+    await start_scheduler(message, state)
 
 
 @dp.message(F.text == 'Вывести дневник')
@@ -112,15 +110,17 @@ async def jobs_change(message: Message, state: FSMContext = None) -> None:
 @dp.message(F.text == 'В определенную дату', ClientState.jobs)
 async def date_jobs(message: Message, state: FSMContext) -> None:
     data = await state.get_data()
+    markup = types.ReplyKeyboardRemove()
     if 'date_jobs' in data:
         await message.answer('Вот ваш список будующих дел:')
         output = "\n".join([f"{key} : {value}" for key, value in data['date_jobs'].items()])
         await message.answer(output)
         await message.answer(
             'Введите дело о котором нужно будет напомнить в определенную дату и оно добавится к списку',
-            reply_markup=types.ReplyKeyboardRemove())
+            reply_markup=markup)
     else:
-        await message.answer('Введите дело о котором нужно будет напомнить в определенную дату')
+        await message.answer('Введите дело о котором нужно будет напомнить в определенную дату',
+            reply_markup=markup)
     await state.set_state(ClientState.date_jobs)
 
 
@@ -133,28 +133,65 @@ async def date_jobs(message: Message, state: FSMContext) -> None:
 
 
 @dp.message(ClientState.date_jobs_2)
-async def date_jobs(message: Message, state: FSMContext) -> None:
+async def date_jobs_2(message: Message, state: FSMContext) -> None:
+    await state.update_data(date=message.text)
+    await message.answer('Сделать сделать это событие регулярным?', reply_markup=create_date_job_keyboard())
+    await state.set_state(ClientState.date_jobs_3)
+
+@dp.message(ClientState.date_jobs_3)
+async def date_jobs_3(message: Message, state: FSMContext) -> None:
+    user_message = message.text.lower().replace('ё', 'е')
     user_states_data = await state.get_data()
+    date = user_states_data['date']
     new_date_jobs = user_states_data['new_date_jobs']
-    if 'date_jobs' in user_states_data:
-        date_jobs = user_states_data['date_jobs']
-        date_jobs[new_date_jobs] = message.text
-    else:
-        date_jobs = {new_date_jobs: message.text}
-    await edit_database(date_jobs=date_jobs)
-    await message.answer('Дело добавлено!')
-    await state.update_data(date_jobs=date_jobs)
-    await state.set_state(ClientState.settings)
-    await settings(message, state)
-    # with open('daily_scores.txt', 'r+', encoding='utf-8-sig') as f:
-    #     data = json.load(f)
-    #     data[str(message.from_user.id)]['date_jobs'][date_jobs] = message.text
-    #     write_to_file(data, f)
-    #
-    #     dict = [key + ' : ' + value for key, value in data[str(message.from_user.id)]['date_jobs'].items()]
-    #     await state.update_data(date_jobs=dict)
-    #     await state.set_state(ClientState.settings)
-    #     await settings(message, state)
+    async def add_date_jobs(new_date_jobs, date):
+        if 'date_jobs' in user_states_data:
+            date_jobs = user_states_data['date_jobs']
+            date_jobs[new_date_jobs] = date
+        else:
+            date_jobs = {new_date_jobs: date}
+        date_jobs = dict(sorted(date_jobs.items(), key=lambda x: x[1]))
+        await edit_database(date_jobs=date_jobs)
+        await state.update_data(date_jobs=date_jobs)
+    if user_message in ['не', 'нет', '-', 'pass', 'пасс', 'не хочу', 'скип',
+                                              'пососи', 'пошел нахуй', 'неа', 'не-а', 0]:
+        await add_date_jobs(new_date_jobs, date)
+        await message.answer('Дело добавлено!')
+    elif user_message == 'каждую неделю':
+        await message.answer('Данное событие будет появлятся в списке разовых дел каждую неделю')
+        async def each_week(new_date_jobs, date):
+            await add_date_jobs(new_date_jobs, date)
+            date = datetime.now().strftime('%Y-%m-%d')
+            await state.set_state(ClientState.settings)
+            await settings(message, state)
+            # await asyncio.sleep(604800)
+            await asyncio.sleep(5)
+            await each_week(new_date_jobs, date)
+            print('новая итерация')
+        await each_week(new_date_jobs, date)
+    elif user_message == 'каждый месяц':
+        await message.answer('Данное событие будет появлятся в списке разовых дел каждый месяц')
+        async def each_month(new_date_jobs, date):
+            await add_date_jobs(new_date_jobs, date)
+            date = datetime.now().strftime('%Y-%m-%d')
+            await state.set_state(ClientState.settings)
+            await settings(message, state)
+            await asyncio.sleep(2592000)
+            # await asyncio.sleep(2592000)
+            await each_week(new_date_jobs, date)
+        await each_month(new_date_jobs, date)
+    elif user_message == 'каждый год':
+        await message.answer('Данное событие будет появлятся в списке разовых дел каждый год')
+        async def each_year(new_date_jobs, date):
+            await add_date_jobs(new_date_jobs, date)
+            date = datetime.now().strftime('%Y-%m-%d')
+            await state.set_state(ClientState.settings)
+            await settings(message, state)
+            await asyncio.sleep(31536000)
+            # await asyncio.sleep(31536000)
+            await each_week(new_date_jobs, date)
+        await each_year(new_date_jobs, date)
+
 
 
 @dp.message(F.text == 'Разовые дела', ClientState.jobs)
@@ -227,10 +264,6 @@ async def command_start(message: Message, state: FSMContext):
             daily_scores[one_jobs[0]] = one_jobs[1]
     await state.update_data(daily_scores=daily_scores)
     await edit_database(daily_scores=daily_scores)
-    # with open('daily_scores.txt', 'r+', encoding='utf-8-sig') as f:
-    #     data = json.load(f)
-    #     data[str(message.from_user.id)]['daily_scores'] = daily_scores
-    #     write_to_file(data, f)
     await message.answer('Отлично, ваш список ежедневных дел обновлен!')
     await state.set_state(ClientState.settings)
     await settings(message, state)
@@ -341,18 +374,34 @@ async def existing_user(message, state):
             + 'Вы можете изменить списки в любой момент')
     if 'one_time_jobs' in user_data:
         one_time_jobs = user_data['one_time_jobs']
-        await message.answer(
-            'Разовые дела:')
+        if 'date_jobs' in user_data:
+            date_jobs = user_data['date_jobs']
+            date_jobs_copy = copy.copy(date_jobs)
+            await message.answer(
+                'Разовые дела:')
+            await message.answer(', '.join(one_time_jobs))
+            async def getting_today_tasks(task, date, date_jobs_copy):
+                time_before = (datetime.now() - datetime.strptime(date, '%Y-%m-%d') - timedelta(
+                    hours=8)).total_seconds()
+                if time_before < 0:
+                    await asyncio.sleep(time_before*-1)
+                else:
+                    del date_jobs_copy[task]
+                    return task, date_jobs_copy
 
-        await message.answer(', '.join(one_time_jobs))
+            for task, date in date_jobs.items():
+                task, date_jobs_copy = await getting_today_tasks(task, date, date_jobs_copy)
+                await message.answer(f'ТОЛЬКО СЕГОДНЯ:\n{task}')
+                await edit_database(date_jobs=date_jobs_copy)
+                await state.update_data(date_jobs=date_jobs_copy)
+                one_time_jobs.append(task)
+                await edit_database(one_time_jobs=one_time_jobs)
+                await state.update_data(one_time_jobs=one_time_jobs)
     await state.set_state(ClientState.greet)
 
 
 async def handle_new_user(message: Message, state):
     info = await bot.get_me()
-    # with open('daily_scores.txt', 'w') as f:
-    #     json.dump({int(message.from_user.id): {'daily_scores': [], 'one_time_jobs': [], 'date_jobs': {}}}, f,
-    #               ensure_ascii=False, indent=4)
     markup = types.ReplyKeyboardRemove()
     await message.answer_sticker('CAACAgIAAxkBAAIsZGVY5wgzBq6lUUSgcSYTt99JnOBbAAIIAAPANk8Tb2wmC94am2kzBA')
     await message.answer(
@@ -366,15 +415,20 @@ async def handle_new_user(message: Message, state):
     await state.set_state(ClientState.new_daily_scores)
 
 
-def start_scheduler(message, state):
-    if not hasattr(start_scheduler, "called"):
-        scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
-        scheduler.add_job(greetings, 'cron', hour=8, minute=00, args=(message, state))
-        scheduler.start()
+async def start_scheduler(message, state):
+    global already_started
+    if already_started:
+        return
+    already_started = True
+    scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
+    scheduler.add_job(greetings, 'cron', hour=8, minute=00, args=(message, state))
+    scheduler.start()
+
+
 
 
 async def main():
-    dp.startup.register(on_startup)
+    await db_start()
     await dp.start_polling(bot)
 
 
